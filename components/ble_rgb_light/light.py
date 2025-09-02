6e3e576c2036f91c1d0bf9663ea84686d66de94e
Geoffrey Prytherch <3165098+gsp8181@users.noreply.github.com>
2025-09-02 20:52:20 +0100

    Fix config

--- components/ble_rgb_light/light.py
+++ components/ble_rgb_light/light.py
@@ -1,3 +1,4 @@
+from logging import config
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import light, output
@@ -7,13 +8,9 @@ empty_light_ns = cg.esphome_ns.namespace('empty_light')
 EmptyLightOutput = empty_light_ns.class_('EmptyLightOutput', light.LightOutput)
 
 CONFIG_SCHEMA = light.BRIGHTNESS_ONLY_LIGHT_SCHEMA.extend({
-    cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(EmptyLightOutput),
-    cv.Required(CONF_OUTPUT): cv.use_id(output.FloatOutput)
+        cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(EmptyLightOutput),
 })
 
-def to_code(config):
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
-    yield light.register_light(var, config)
-
-    out = yield cg.get_variable(config[CONF_OUTPUT])
-    cg.add(var.set_output(out))
+    await light.register_light(var, config)
